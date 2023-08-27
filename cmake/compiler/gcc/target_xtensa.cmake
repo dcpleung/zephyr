67d2f55c5182f47926fe2ef205f511bcf1474975
Andy Ross <andyross@google.com>
2023-08-27 09:05:06 -0700

    arch/xtensa: Add XTENSA_CALL0_ABI kconfig
    New ABI, much simpler, slightly bigger/slower code, but MUCH cleaner
    syscall entry.
    
    Signed-off-by: Andy Ross <andyross@google.com>
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>

--- cmake/compiler/gcc/target_xtensa.cmake
+++ cmake/compiler/gcc/target_xtensa.cmake
@@ -3,4 +3,7 @@
 if(CONFIG_XTENSA_WINDOWED_ABI)
   list(APPEND TOOLCHAIN_C_FLAGS -mabi=windowed)
   list(APPEND TOOLCHAIN_LD_FLAGS -mabi=windowed)
+elseif(CONFIG_XTENSA_CALL0_ABI)
+  list(APPEND TOOLCHAIN_C_FLAGS -mabi=call0)
+  list(APPEND TOOLCHAIN_LD_FLAGS -mabi=call0)
 endif()
